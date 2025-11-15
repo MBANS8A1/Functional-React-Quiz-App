8d201361a4c3f1bd2b74dd24e7a03ecce8f7a0b3
MBANS8A1 <seanmbng@aol.com>
2025-11-15 21:25:01 +0000

    I have modified the reducer function to account for the different action types related to the count (increasing, decreasing and setting). I also applied the action object to the defineCount function variable, which is applied to the onChange of the input element.

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,7 +2,9 @@ import { useState, useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  return state + action;
+  if (action.type === "inc") return state + action.payload;
+  if (action.type === "dec") return state - action.payload;
+  if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
@@ -29,6 +31,7 @@ function DateCounter() {
 
   const defineCount = function (e) {
     // setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
