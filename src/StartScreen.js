9803c5ebe90045e05a58b966f1ce6e73ff70c371
MBANS8A1 <seanmbng@aol.com>
2025-11-17 22:44:00 +0000

    I added some extra CSS for the h2 and h3. Also, for the div container in the StartScreen component, I added CSS for it so it has a column value for the flex-direction and the contained elements are aligned vertically in the center.

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,5 +1,10 @@
 function StartScreen() {
-  return <div></div>;
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz!</h2>
+      <h3>X questions to test your React mastery!</h3>
+    </div>
+  );
 }
 
 export default StartScreen;
