8a1c8ed917b361ceb69cf92170b5ec02559d2599
MBANS8A1 <seanmbng@aol.com>
2025-11-18 20:09:11 +0000

    In the Options component, I added an onClick attribute to the button element rendered, which will dispatch an action with the index of matching the correctOption of the question; this is signified as the payload.

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,12 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
   return (
     <div className="options">
-      {question.options.map((qOption) => (
-        <button className="btn btn-option" key={qOption}>
+      {question.options.map((qOption, index) => (
+        <button
+          className="btn btn-option"
+          key={qOption}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {qOption}
         </button>
       ))}
