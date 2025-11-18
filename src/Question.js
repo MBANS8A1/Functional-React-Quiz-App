41bd01bfc3bbdfff10ca8f68c78154e8f2666268
MBANS8A1 <seanmbng@aol.com>
2025-11-18 14:41:47 +0000

    Instead of an ul element with nested li elements, I decided that each option the user can choose from as a possible answer for a question will be a button. The implicit parameter inside the map function I added was name as qOption (denoting the possible option for the user to choose from). I will next add the stlying for the .options div and rendered button for each qOption.

--- src/Question.js
+++ src/Question.js
@@ -3,6 +3,11 @@ function Question({ question }) {
   return (
     <div>
       <h4>{question.question}</h4>
+      <div className="options">
+        {question.options.map((qOption) => (
+          <button className="btn btn-option">{qOption}</button>
+        ))}
+      </div>
     </div>
   );
 }
