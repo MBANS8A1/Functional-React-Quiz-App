a54ab311d1159bc58f6ef71226927c21881fa0df
MBANS8A1 <seanmbng@aol.com>
2025-11-19 17:18:13 +0000

    If there is no answer, then no NextButton instance is rendered. If there is an answer the component will return a button, which when clicked will dispatch an action object of type "nextQuestion", which will increment the index state be 1.

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,5 +1,13 @@
 function NextButton({ dispatch, answer }) {
-  return <div></div>;
+  if (answer === null) return null;
+  return (
+    <button
+      className="btn btn-ui"
+      onClick={() => dispatch({ type: "nextQuestion" })}
+    >
+      Next
+    </button>
+  );
 }
 
 export default NextButton;
