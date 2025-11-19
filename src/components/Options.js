a78141af18ca0d153d3836aaed837b32e34a306c
MBANS8A1 <seanmbng@aol.com>
2025-11-19 11:16:49 +0000

    The conditional formatting for colouring the buttons in the .correct or .wrong classNames should not happen when an answer has not been chosen yet. So I created a variable called hasAnswered and used it on line 8 to create a nested ternary statement. So now when the question appears there is no clue as to what is the right answer.

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,10 +1,15 @@
 function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
       {question.options.map((qOption, index) => (
         <button
           className={`btn btn-option ${index === answer ? "answer" : ""} ${
-            index === question.correctOption ? "correct" : "wrong"
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
           }`}
           key={qOption}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
