13e5a5edb20fc6cd8251b24d76946f7d5265383d
MBANS8A1 <seanmbng@aol.com>
2025-11-19 18:20:51 +0000

    When the Next button was clicked the application did correctly move to the next question but the organge and green (.correct/.wrong) formatting was still applied. As a result, the answer was already given. To fix this I reset th answer back to null in within the newAnswer case inside of the reducer function.

--- src/components/App.js
+++ src/components/App.js
@@ -40,7 +40,7 @@ function reducer(state, action) {
       };
 
     case "nextQuestion":
-      return { ...state, index: state.index + 1 };
+      return { ...state, index: state.index + 1, answer: null };
 
     default:
       throw new Error("Action is unknown!");
