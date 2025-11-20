6db8a21be7661db915307f15d125d27f524d3441
MBANS8A1 <seanmbng@aol.com>
2025-11-20 13:04:18 +0000

    If the index is equal to the number of questions minus 1 then the "Finish" button should be rendered , which when clciked will dispatch a different action object of type "finish".

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -10,6 +10,16 @@ function NextButton({ dispatch, answer, index, numQuestions }) {
       </button>
     );
   }
+  if (index === numQuestions - 1) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
+  }
 }
 
 export default NextButton;
