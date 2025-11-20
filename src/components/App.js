e9a82145f5c01fff2709d1d01012c1e7fbed6874
MBANS8A1 <seanmbng@aol.com>
2025-11-20 15:59:29 +0000

    I decided to update the highscore state property when the game has finished. The highscore's value will depend on whether the current points from state.points exceeds it or not.

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -46,7 +47,12 @@ function reducer(state, action) {
       return { ...state, index: state.index + 1, answer: null };
 
     case "finish":
-      return { ...state, status: "finished" };
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
 
     default:
       throw new Error("Action is unknown!");
