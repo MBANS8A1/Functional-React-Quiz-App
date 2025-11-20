480f3b1eb8b28f32c55f77d1ff145290bb468503
MBANS8A1 <seanmbng@aol.com>
2025-11-20 11:56:25 +0000

    I calculated the points out of the total points using the scorePercentage; it is then rounded up to the nearest integer.

--- src/components/FinishedScreen.js
+++ src/components/FinishedScreen.js
@@ -1,7 +1,9 @@
-function FinishedScreen() {
+function FinishedScreen({ points, maxPossiblePoints }) {
+  const scorePercentage = (points / maxPossiblePoints) * 100;
   return (
     <p>
-      You scored <strong>{points}</strong>
+      You scored <strong>{points}</strong> out of {maxPossiblePoints}{" "}
+      (Math.ceil(scorePercentage))
     </p>
   );
 }
