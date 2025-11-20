803ab2b1cb6eeb99c5c030783490c98ef5a92115
MBANS8A1 <seanmbng@aol.com>
2025-11-20 10:19:43 +0000

    I added a progress html element within the header element of the Progress function component. The bar starts from index, as I want the 1st question to start from 0 as that question would have not been answered yet.

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,6 +1,7 @@
 function Progress({ index, numQuestions, points, maxPossiblePoints }) {
   return (
     <header className="progress">
+      <progress max={numQuestions} value={index}></progress>
       <p>
         Question <strong>{index + 1}</strong>/{numQuestions}
       </p>
