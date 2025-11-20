0581e270e898897a56998a98ba549df4a301f9c0
MBANS8A1 <seanmbng@aol.com>
2025-11-20 12:11:25 +0000

    I applied the .result className to the p element in the FinishedScreen component and resolved a problem with Math.ceil.

--- src/components/FinishedScreen.js
+++ src/components/FinishedScreen.js
@@ -1,9 +1,9 @@
 function FinishedScreen({ points, maxPossiblePoints }) {
   const scorePercentage = (points / maxPossiblePoints) * 100;
   return (
-    <p>
-      You scored <strong>{points}</strong> out of {maxPossiblePoints}{" "}
-      (Math.ceil(scorePercentage))
+    <p className="result">
+      You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+      {Math.ceil(scorePercentage)})
     </p>
   );
 }
