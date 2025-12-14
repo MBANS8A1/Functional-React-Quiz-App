2bc792b38adccc8043e7bf3d66263209d2c8dd70
MBANS8A1 <seanmbng@aol.com>
2025-12-14 21:04:52 +0000

    I just added an extra word to the p element in the Error.js file.

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,7 @@
 function Error() {
   return (
     <p className="error">
-      <span>💥</span> There was an error fetching questions.
+      <span>💥</span> There was an error the fetching questions.
     </p>
   );
 }
