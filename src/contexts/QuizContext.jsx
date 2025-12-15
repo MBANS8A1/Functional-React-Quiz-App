748f59bdf9f6ee2fa80d3a49d05717b9f32713a5
MBANS8A1 <seanmbng@aol.com>
2025-12-15 11:14:04 +0000

    I moved the majoirty of the logic over from App.js to QuizContext.js (and I renamed the extension of the file). I am returning QuizProvider.Context with the children components that will be supplied the values. Also, I made a useQuiz custom hook so components can consume the context values.

--- src/contexts/QuizContext.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext, useReducer } from "react";
-
-const QuizContext = createContext();
-
-function reducer(state, action) {}
-function QuizProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-}
